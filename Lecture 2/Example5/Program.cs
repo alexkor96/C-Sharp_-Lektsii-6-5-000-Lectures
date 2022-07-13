b66073ad450b4d1152c66f3ea514072f90c46f1e
alex <alex.korolev96@gmail.com>
2022-07-13 23:35:20 +0300

    пример с поиском индекса цисла в случайно заданном массиве

---
+++ Lecture 2/Example5/Program.cs
@@ -0,0 +1,42 @@
+﻿void FillArray(int [] collection)
+{
+    int Lenght = collection.Length;
+    int index = 0;
+    while(index < Lenght)
+    {
+        collection[index] = new Random().Next(1,10);
+        index++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int Count = col.Length;
+    int position = 0;
+    while(position< Count){
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+int IndexOf(int [] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0; 
+    int position = -1;
+    while(index < count){
+        if (collection[index] == find)
+        {
+            position = index; 
+            break;
+        }
+        index++;
+    }
+
+    return position;
+}
+
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int Pos = IndexOf(array, 4); 
+Console.WriteLine(Pos);
