e340b65c6a31208f3ecabf1fba08bbdfae0b22a5
alex <alex.korolev96@gmail.com>
2022-07-13 22:44:37 +0300

    пример на описание метода void, для создания массива, из случайных псевдо чисел и метода печати массива в констоль

---
+++ Lecture 2/Example4/Program.cs
@@ -0,0 +1,25 @@
+﻿void FillArray(int [] collection)
+{
+    int Lenght = collection.Length;
+    int index = 0;
+    while(index < Lenght)
+    {
+        collection[index] = new Random().Next(1,10);
+        index++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int Count = col.Length;
+    int position = 0;
+    while(position< Count){
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+
+
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
