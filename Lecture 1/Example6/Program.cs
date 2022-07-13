8cff50bdd63bd3c490f54e15775a72998c13d94e
alex <alex.korolev96@gmail.com>
2022-07-13 20:39:19 +0300

    example 6 Максимум из 5 чисел

---
+++ Lecture 1/Example6/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 10; 
+int b = 34;
+int c = 43;
+int d = 26;
+int e = 64;
+int Max = a;
+
+if (b > Max) Max = b;
+if (c > Max) Max = c;
+if (d > Max) Max = d;
+if (e > Max) Max = e;
+
+Console.WriteLine(Max);
+
