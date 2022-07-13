7392889f36b391aeca08ca04fbe6bf517d792393
alex <alex.korolev96@gmail.com>
2022-07-13 19:47:35 +0300

    example 5 Особое приветствие Алексея!

---
+++ Lecture 1/Example5/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите Ваше имя: ");
+string username = Convert.ToString(Console.ReadLine()); 
+if(username.ToLower() == "алексей"){
+Console.WriteLine("Приветствую тебя, Воин!");
+}
+else Console.WriteLine("Привет, " + username);
