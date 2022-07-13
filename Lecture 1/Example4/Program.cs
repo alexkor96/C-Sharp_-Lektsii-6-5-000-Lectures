9ed60a53a86841df7f71bd8c87a657ee7e1c8558
alex <alex.korolev96@gmail.com>
2022-07-13 19:05:46 +0300

    example 4 Часное двух чисел

---
+++ Lecture 1/Example4/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите первое число");
+double NumberA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите первое число");
+double NumberB = Convert.ToDouble(Console.ReadLine());
+double Div = NumberA/NumberB;
+Console.WriteLine("Часное двух чисех = " + Div);
