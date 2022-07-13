f4edc4e503c849c36565faa02bea40fc3ebfd577
alex <alex.korolev96@gmail.com>
2022-07-13 18:50:55 +0300

    example 3 sum

---
+++ Lecture 1/Example3/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите первое число");
+int NumberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число");
+int NumberB = Convert.ToInt32(Console.ReadLine());
+int Sum = NumberA + NumberB;
+Console.WriteLine("Сумма двух чисел = " + Sum);
