9082e93a7676a95a4f2c676c1181444a0e70900c
alex <alex.korolev96@gmail.com>
2022-07-13 21:55:30 +0300

    новый пример поиска максимума из 9 чисел с использованием массива

---
+++ Lecture 2/Example2/Program.cs
@@ -0,0 +1,15 @@
+﻿int Max(int Arg1, int Arg2, int Arg3) {
+    int resuslt = Arg1;
+    if(Arg2>resuslt) resuslt = Arg2;
+    if(Arg3>resuslt) resuslt = Arg3;
+    return resuslt;
+}
+int [] array = {112 , 212 , 31 , 425 , 581 , 69, 726 , 118 , 93};
+
+int max = Max (
+Max(array[0],array[1],array[2]),
+Max(array[3],array[4],array[5]),
+Max(array[6],array[7],array[8])
+);
+
+Console.WriteLine(max);
