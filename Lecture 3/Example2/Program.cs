e055e4d6f42954d7b5fc1d23f47b0a5a742d7f3d
alex <alex.korolev96@gmail.com>
2022-07-14 14:00:53 +0300

    пример использования цикла For

---
+++ Lecture 3/Example2/Program.cs
@@ -0,0 +1,13 @@
+﻿// пример использования цикла For
+string Method4(int count, string C){
+     
+    // string result = " "
+    string result = String.Empty;
+    for(int i =0; i< count; i++)
+    {
+        result = result + C;
+    }
+    return result;
+}
+string res = Method4( 10, "a");
+Console.WriteLine(res);
