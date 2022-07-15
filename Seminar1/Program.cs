9bd5d236d9e8df23529dff7ba70b8d97a9c07861
alex <alex.korolev96@gmail.com>
2022-07-15 13:16:44 +0300

    Добавлена первая задача на поиск Макс. из двух заданных чисел.

---
+++ Seminar1/Program.cs
@@ -0,0 +1,9 @@
+﻿//Написать программу которая принимает число и выдает его квадрат. 
+
+//Console.WriteLine("Введите число: ");
+//int square = 0;
+//int n = Convert.ToInt32(Console.ReadLine());
+//square = n*n;
+//Console.WriteLine("Квадрат числа = " + square);
+
+
